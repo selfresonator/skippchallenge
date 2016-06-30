4619a4b3e4e8cd30817502e8d5a677fbede0ca09
selfresonator <selfresonator@gmail.com>
2016-06-30 11:45:01 -0700

    Builds site architecture, basic controllers

---
+++ app/js/search/search.controller.js
@@ -0,0 +1,4 @@
+angular.module('search.controller', [])
+.controller('SearchCtrl', function($scope) {
+  $scope.messge = 'Hello';
+});
---
+++ app/js/services/data.service.js
@@ -0,0 +1,2 @@
+angular.module('data.service', ['SearchCtrl'])
+.factory()
