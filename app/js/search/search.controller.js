c0e54947e7642a3ad7bf35343dc310e91bf20ca9
selfresonator <selfresonator@gmail.com>
2016-06-30 14:46:33 -0700

    Shows all restaurants

--- app/js/search/search.controller.js
+++ app/js/search/search.controller.js
@@ -1,4 +1,18 @@
 angular.module('search.controller', [])
-.controller('SearchCtrl', function($scope, $route) {
-  $scope.message = 'Hello';
+.controller('SearchCtrl', function($scope, $route, SearchService) {
+  $scope.restaurants = [];
+  $scope.place = '';
+  $scope.clientQuery = function(place) {
+    SearchService.searchCity(place)
+      .then(function(data) {
+        $scope.restaurants[0] = data.data.results[0];
+        $scope.restaurants[1] = data.data.results[1];
+        $scope.restaurants[2] = data.data.results[2];
+        $scope.restaurants[3] = data.data.results[3];
+        console.log($scope.restaurants);
+      });
+  };
 });
+
+
+// name, picture, two other attributes
--- app/js/services/data.service.js
+++ app/js/services/data.service.js
@@ -1,10 +1,13 @@
-angular.module('data.service', ['SearchCtrl'])
+angular.module('data.service', [])
 .factory('SearchService', function($http) {
-  function(cityName) {
+  function searchCity(place) {
+    var key = 'AIzaSyDxgRA2vLaeap_nOCPHDsI1Nm2v1zHKr10';
     return $http({
       method: 'GET',
-      url: '',
-      city: cityName
+      url: 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+'+place+'&key='+key+'',
+      headers: {
+         'Content-Type': undefined
+       },
     });
   }
 
