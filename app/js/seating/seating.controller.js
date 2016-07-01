d60a158c5da494d6b8868f72026d4304d5febc71
selfresonator <selfresonator@gmail.com>
2016-06-30 20:03:48 -0700

    Completes 95% of features

--- app/js/seating/seating.controller.js
+++ app/js/seating/seating.controller.js
@@ -2,10 +2,23 @@ angular.module('seating.controller', [])
 .controller('SeatingCtrl', function($scope) {
   $scope.size = 0;
   $scope.selected = false;
-  $scope.chooseSize = function(num) {
+  $scope.selectSize = function(num) {
     $scope.size = num;
     $scope.selected = true;
-    console.log($scope.size);
   };
 
+  $scope.timeReserved;
+  $scope.squareSelect = true;
+  $scope.selectSeat = function() {
+    if ($scope.squareSelect === true) {
+      $scope.timeReserved = new moment().format('MMMM Do YYYY, h:mm:ss a');
+    } else {
+      $scope.timeReserved = $scope.timeReserved;
+    }
+  };
+
+
+})
+.directive('isolate', function() {
+    return {scope: true};
 });
